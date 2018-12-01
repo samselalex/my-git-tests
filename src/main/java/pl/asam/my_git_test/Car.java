5b8b1d1f817b712a0926a2d01f1090af178c253c
Alex PC <samsel.aleksander@gmail.com>
2018-12-01 11:42:50 +0100

    [add] - dodanie zmian w Car

--- src/main/java/pl/asam/my_git_test/Car.java
+++ src/main/java/pl/asam/my_git_test/Car.java
@@ -4,6 +4,19 @@ public class Car {
     private String name;
     private String color;
 
+    public Car(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
     public String getName() {
         return name;
     }
